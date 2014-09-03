09db089eec27da7d655ebdaeeeb545cc2458f0f2
Eunchong Yu <kroisse@gmail.com>
2014-09-03 12:41:54 +0900

    Remove XmlDecoder
    yay!

--- src/parser/base.rs
+++ src/parser/base.rs
@@ -1,17 +1,11 @@
 use xml;
-use xml::reader::events::{XmlEvent, EndDocument, StartElement, EndElement, Error};
+use xml::reader::events as x;
 
 use schema;
 
-pub struct XmlDecoder<B> {
-    reader: xml::EventReader<B>,
-    peeked: Option<(XmlEvent, uint)>,
-    depth: uint,
-}
-
 pub enum DecodeError {
     XmlError(xml::common::Error),
-    UnexpectedEvent { event: XmlEvent, depth: uint },
+    UnexpectedEvent { event: x::XmlEvent, depth: uint },
     NoResult,
     AttributeNotFound(String),
     SchemaError(schema::SchemaError),
@@ -19,75 +13,6 @@ pub enum DecodeError {
 
 pub type DecodeResult<T> = Result<T, DecodeError>;
 
-impl<B: Buffer> XmlDecoder<B> {
-    pub fn new(reader: xml::EventReader<B>) -> XmlDecoder<B> {
-        XmlDecoder { reader: reader, peeked: None, depth: 0 }
-    }
-
-    fn _next(&mut self) -> (XmlEvent, uint) {
-        let v = self.reader.next();
-        let depth = match &v {
-            &StartElement { .. } => { self.depth += 1; self.depth }
-            &EndElement { .. } => { let d = self.depth; self.depth -= 1; d }
-            _ => { self.depth }
-        };
-        (v, depth)
-    }
-
-    #[inline]
-    fn next(&mut self) -> (XmlEvent, uint) {
-        if self.peeked.is_some() { self.peeked.take().unwrap() }
-        else { self._next() }
-    }
-
-    #[inline]
-    fn peek<'a>(&'a mut self) -> &'a (XmlEvent, uint) {
-        if self.peeked.is_none() {
-            self.peeked = Some(self._next());
-        }
-        self.peeked.get_ref()
-    }
-
-    fn drain_children(&mut self, depth: uint) -> DecodeResult<(XmlEvent, uint)> {
-        loop {
-            match self.next() {
-                (Error(e), _) => { return Err(XmlError(e)); }
-                (_, d) if d > depth => { continue; }
-                (evt @ EndElement { .. }, d) => { return Ok((evt, d)); }
-                (evt, d) => { return Err(UnexpectedEvent { event: evt, depth: d }); }
-            }
-        }
-    }
-
-    pub fn read_event<T>(&mut self, f: |&mut XmlDecoder<B>, XmlEvent| -> DecodeResult<T>) -> DecodeResult<T> {
-        match self.next() {
-            (evt @ StartElement { .. }, depth) => {
-                let result = f(self, evt);
-                try!(self.drain_children(depth));
-                result
-            }
-            (evt @ EndElement { .. }, depth) => {
-                Err(UnexpectedEvent { event: evt, depth: depth })
-            }
-            (evt, _) => { f(self, evt) }
-        }
-    }
-
-    pub fn each_child(&mut self, f: |&mut XmlDecoder<B>| -> DecodeResult<()>) -> DecodeResult<()> {
-        loop {
-            match *self.peek().ref0() {
-                Error(ref e) => { return Err(XmlError(e.clone())); }
-                EndDocument => { return Ok(()); }
-                EndElement { .. } => { return Ok(()); }
-                _ => { }
-            }
-            match f(self) {
-                Ok(()) => { }
-                Err(e) => { return Err(e); }
-            }
-        }
-    }
-}
 
 pub struct NestedEventReader<'a, B> {
     reader: &'a mut xml::EventReader<B>,
@@ -104,7 +29,6 @@ impl<'a, B: Buffer> NestedEventReader<'a, B> {
         if self.finished {
             None
         } else {
-            use xml::reader::events as x;
             use self::events as n;
             let ev = match self.reader.next() {
                 x::StartDocument { version, encoding, standalone } =>
@@ -127,7 +51,7 @@ impl<'a, B: Buffer> NestedEventReader<'a, B> {
                     }
                 }
 
-                x::EndElement { name } => {
+                x::EndElement { .. } => {
                     self.finished = true;
                     return None;
                 }
@@ -154,7 +78,7 @@ impl<'a, B: Buffer + Send> Drop for NestedEventReader<'a, B> {
         // drain all remained events
         loop {
             match self.reader.next() {
-                EndDocument | EndElement { .. } | Error(_) => break,
+                x::EndDocument | x::EndElement { .. } | x::Error(_) => break,
                 _ => { }
             }
         }
