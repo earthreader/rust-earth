ff55574d3dcddf0e8495e7eb664f1e2613e51c7d
Eunchong Yu <kroisse@gmail.com>
2015-01-25 11:01:12 +0900

    Update nightly doc
    Signed-off-by: Eunchong Yu <kroisse@gmail.com>

---
+++ nightly/implementors/core/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['earth'] = ["<a class='stability Unstable' title='Unstable: the exact API of this trait may change'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/error/trait.Error.html' title='core::error::Error'>Error</a> for Error","<a class='stability Unstable' title='Unstable: the exact API of this trait may change'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/error/trait.Error.html' title='core::error::Error'>Error</a> for <a class='enum' href='earth/parser/base/enum.DecodeError.html' title='earth::parser::base::DecodeError'>DecodeError</a>","<a class='stability Unstable' title='Unstable: the exact API of this trait may change'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/error/trait.Error.html' title='core::error::Error'>Error</a> for <a class='enum' href='earth/repository/enum.RepositoryError.html' title='earth::repository::RepositoryError'>RepositoryError</a>","<a class='stability Unstable' title='Unstable: the exact API of this trait may change'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/error/trait.Error.html' title='core::error::Error'>Error</a> for <a class='enum' href='earth/schema/enum.SchemaError.html' title='earth::schema::SchemaError'>SchemaError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- nightly/implementors/core/iter/trait.FromIterator.js
+++ nightly/implementors/core/iter/trait.FromIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['earth'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;<a class='struct' href='earth/feed/link/struct.Link.html' title='earth::feed::link::Link'>Link</a>&gt; for <a class='struct' href='earth/feed/link/struct.LinkList.html' title='earth::feed::link::LinkList'>LinkList</a>",];
+implementors['earth'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;<a class='struct' href='earth/feed/struct.Link.html' title='earth::feed::Link'>Link</a>&gt; for <a class='struct' href='earth/feed/struct.LinkList.html' title='earth::feed::LinkList'>LinkList</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
