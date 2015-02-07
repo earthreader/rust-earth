0362a7d094a1c478800e2fc9314c8e2b882867b8
Eunchong Yu <kroisse@gmail.com>
2015-02-07 21:36:10 +0900

    Assume all paths in the earthreader repo are utf-8 encodeded string

--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -12,7 +12,6 @@
 use std::error::{Error, FromError};
 use std::fmt;
 use std::old_io::IoError;
-use std::old_path::BytesContainer;
 
 pub use self::utils::{Bytes, Names};
 pub use self::fs::FileSystemRepository;
@@ -23,7 +22,7 @@ pub type RepositoryResult<T> = Result<T, RepositoryError>;
 
 #[derive(Debug)]
 pub enum RepositoryError {
-    InvalidKey(Vec<Vec<u8>>, Option<IoError>),
+    InvalidKey(Vec<String>, Option<IoError>),
     InvalidUrl(&'static str),
     NotADirectory(Path),
     CannotBorrow,
@@ -32,11 +31,11 @@ pub enum RepositoryError {
 
 impl RepositoryError {
     #[inline]
-    pub fn invalid_key<T: BytesContainer>(key: &[T], cause: Option<IoError>) ->
+    pub fn invalid_key<T: Str>(key: &[T], cause: Option<IoError>) ->
         RepositoryError
     {
         let copied_key = key.iter()
-            .map(|e| e.container_as_bytes().to_vec())
+            .map(|e| e.as_slice().to_string())
             .collect();
         RepositoryError::InvalidKey(copied_key, cause)
     }
@@ -117,18 +116,18 @@ impl FromError<IoError> for RepositoryError {
 /// ```
 pub trait Repository {
     /// Read the content from the `key`.
-    fn get_reader<'a, T: BytesContainer>(&'a self, key: &[T]) ->
+    fn get_reader<'a, T: Str>(&'a self, key: &[T]) ->
         RepositoryResult<Box<Buffer + 'a>>;
 
     /// Get a writer to write data into the ``key``.
-    fn get_writer<'a, T: BytesContainer>(&'a mut self, key: &[T]) ->
+    fn get_writer<'a, T: Str>(&'a mut self, key: &[T]) ->
         RepositoryResult<Box<Writer + 'a>>;
 
-    fn read<T: BytesContainer>(&self, key: &[T]) -> RepositoryResult<Vec<u8>> {
+    fn read<T: Str>(&self, key: &[T]) -> RepositoryResult<Vec<u8>> {
         Ok(try!(try!(self.get_reader(key)).read_to_end()))
     }
 
-    fn write<T: BytesContainer, U: Bytes>(&mut self, key: &[T], buf: &[U]) ->
+    fn write<T: Str, U: Bytes>(&mut self, key: &[T], buf: &[U]) ->
         RepositoryResult<()>
     {
         let mut w = try!(self.get_writer(key));
@@ -139,11 +138,10 @@ pub trait Repository {
     }
 
     /// Return whether the `key` exists or not.
-    fn exists<T: BytesContainer>(&self, key: &[T]) -> bool;
+    fn exists<T: Str>(&self, key: &[T]) -> bool;
 
     /// List all subkeys in the `key`.
-    fn list<'a, T: BytesContainer>(&'a self, key: &[T]) ->
-        RepositoryResult<Names<'a>>;
+    fn list<'a, T: Str>(&'a self, key: &[T]) -> RepositoryResult<Names<'a>>;
 }
 
 pub trait ToRepository<R: Repository> {
@@ -160,7 +158,7 @@ pub trait ToRepository<R: Repository> {
 }
 
 mod utils {
-    pub type Names<'a> = Box<Iterator<Item=Vec<u8>> + 'a>;
+    pub type Names<'a> = Box<Iterator<Item=String> + 'a>;
 
     pub trait Bytes {
         fn as_bytes<'a>(&'a self) -> &'a [u8];
@@ -192,37 +190,35 @@ mod utils {
 pub mod test {
     use super::{Names, Repository, RepositoryError, RepositoryResult};
 
+    use std::borrow::ToOwned;
     use std::collections::BTreeSet;
     use std::old_io::fs::PathExtensions;
     use std::old_io::util::{NullReader, NullWriter};
-    use std::old_path::BytesContainer;
 
     struct RepositoryImplemented;
     
     impl Repository for RepositoryImplemented {
-        fn get_reader<T: BytesContainer>(&self, _key: &[T]) ->
+        fn get_reader<T: Str>(&self, _key: &[T]) ->
             RepositoryResult<Box<Buffer>>
         {
             Ok(Box::new(NullReader) as Box<Buffer>)
         }
 
-        fn get_writer<T: BytesContainer>(&mut self, _key: &[T]) ->
+        fn get_writer<T: Str>(&mut self, _key: &[T]) ->
             RepositoryResult<Box<Writer>>
         {
             Ok(Box::new(NullWriter) as Box<Writer>)
         }
 
-        fn exists<T: BytesContainer>(&self, _key: &[T]) -> bool {
+        fn exists<T: Str>(&self, _key: &[T]) -> bool {
             true
         }
 
-        fn list<T: BytesContainer>(&self, _key: &[T]) ->
-            RepositoryResult<Names>
-        {
+        fn list<T: Str>(&self, _key: &[T]) -> RepositoryResult<Names> {
             struct Empty<'a>;
             impl<'a> Iterator for Empty<'a> {
-                type Item = Vec<u8>;
-                fn next(&mut self) -> Option<Vec<u8>> { None }
+                type Item = String;
+                fn next(&mut self) -> Option<String> { None }
                 fn size_hint(&self) -> (usize, Option<usize>) { (0, Some(0)) }
             }
             Ok(Box::new(Empty) as Names)
@@ -246,28 +242,28 @@ pub mod test {
     }
 
     pub fn test_repository<R: Repository>(mut repository: R) {
-        let empty: &[&[u8]] = &[];
+        let empty: &[&str] = &[];
         expect_invalid_key!(repository.get_reader, &[]);
         expect_invalid_key!(repository.get_writer, &[]);
         assert_eq!(unwrap!(repository.list(empty)).next(), None);
         assert!(!repository.exists(&["key"]));
-        expect_invalid_key!(repository.read, &[b"key"]);
-        unwrap!(repository.write(&["key"], &[b"cont", b"ents"]));
+        expect_invalid_key!(repository.read, &["key"]);
+        unwrap!(repository.write(&["key"], &["cont", "ents"]));
         assert_eq!(unwrap!(repository.list(empty)).collect::<Vec<_>>(),
-                   [b"key"]);
+                   ["key"]);
         assert!(repository.exists(&["key"]));
         assert_eq!(unwrap!(repository.read(&["key"])), b"contents");
         assert!(!repository.exists(&["dir", "key"]));
-        expect_invalid_key!(repository.read, &[b"dir", b"key"]);
+        expect_invalid_key!(repository.read, &["dir", "key"]);
         unwrap!(repository.write(&["dir", "key"], &["cont", "ents"]));
         assert_eq!(unwrap!(repository.list(empty)).collect::<BTreeSet<_>>(),
-                   [b"dir", b"key"].iter().map(|b| b.to_vec())
-                                   .collect::<BTreeSet<_>>());
+                   ["dir".to_string(), "key".to_string()]
+                   .iter().map(ToOwned::to_owned).collect::<BTreeSet<_>>());
         assert!(repository.exists(&["dir", "key"]));
         assert!(!repository.exists(&["dir", "key2"]));
         assert_eq!(unwrap!(repository.read(&["dir", "key"])), b"contents");
         // directory test
-        expect_invalid_key!(repository.get_writer, &[b"key", b"key"]);
-        expect_invalid_key!(repository.list, &[b"key"]);
+        expect_invalid_key!(repository.get_writer, &["key", "key"]);
+        expect_invalid_key!(repository.list, &["key"]);
     }
 }
